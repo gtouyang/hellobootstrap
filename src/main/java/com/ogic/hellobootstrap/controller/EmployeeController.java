2ce398b790719b2c4be26c4855d268d4409aaf5f
ogic <ogic@Mint.com>
2019-05-07 15:20:51 +0800

    增加了员工添加功能

--- src/main/java/com/ogic/hellobootstrap/controller/EmployeeController.java
+++ src/main/java/com/ogic/hellobootstrap/controller/EmployeeController.java
@@ -1,11 +1,14 @@
 package com.ogic.hellobootstrap.controller;
 
+import com.ogic.hellobootstrap.dao.DepartmentDao;
 import com.ogic.hellobootstrap.dao.EmployeeDao;
+import com.ogic.hellobootstrap.entities.Department;
 import com.ogic.hellobootstrap.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collection;
 
@@ -15,12 +18,28 @@ public class EmployeeController {
     @Autowired
     EmployeeDao employeeDao;
 
-    @GetMapping("/emps")
+    @Autowired
+    DepartmentDao departmentDao;
+
+    @GetMapping("/emp")
     public String employeeList(Model model){
         Collection<Employee> employees = employeeDao.getAll();
 
         model.addAttribute("emps", employees);
 
-        return "emps/list";
+        return "emp/list";
+    }
+
+    @GetMapping("/add-emp")
+    public String toAddEmployeePage(Model model){
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts",departments);
+        return "emp/add";
+    }
+
+    @PostMapping("/add-emp")
+    public String addEmp(Employee employee){
+        employeeDao.save(employee);
+        return "redirect:/emp";
     }
 }
