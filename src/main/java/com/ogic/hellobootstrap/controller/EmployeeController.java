7518114d102c43d2166ee9f55caca669f70afe9e
ogic <ogic@Mint.com>
2019-05-08 21:22:28 +0800

    增加了员工修改功能

--- src/main/java/com/ogic/hellobootstrap/controller/EmployeeController.java
+++ src/main/java/com/ogic/hellobootstrap/controller/EmployeeController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.Collection;
 
@@ -39,6 +41,22 @@ public class EmployeeController {
 
     @PostMapping("/add-emp")
     public String addEmp(Employee employee){
+        employeeDao.save(employee);
+        return "redirect:/emp";
+    }
+
+    @GetMapping("/emp/{id}")
+    public String toEditPage(@PathVariable("id") Integer id, Model model){
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts",departments);
+        Employee employee =  employeeDao.get(id);
+        model.addAttribute("emp",employee);
+        return "emp/add";
+    }
+
+    @PutMapping("/emp/{id}")
+    public String updateEmp(@PathVariable("id") Integer id, Employee employee){
+        employee.setId(id);
         employeeDao.save(employee);
         return "redirect:/emp";
     }
